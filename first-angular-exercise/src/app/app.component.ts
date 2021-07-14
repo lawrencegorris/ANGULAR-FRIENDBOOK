edec460b08960a4a725b7b6025fbf2f51be91d28
lawrencegorris <gorrislawrence.personal@gmail.com>
2021-07-14 15:50:52 +0200

    Page updates in real-time when adding new friend + added extra field

--- first-angular-exercise/src/app/app.component.ts
+++ first-angular-exercise/src/app/app.component.ts
@@ -1,21 +1,39 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
 import { Friend } from './friend';
 import { AddFriendService } from './add-friend.service';
+import { all } from 'q';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  friendModel = new Friend('', '', '', '', '');
+export class AppComponent{
+  friendModel = new Friend('', '', '', '', '', '');
+  allFriends: any;
 
   sendData = () => {
-    console.log(this.friendModel);
-    this.addFriendService.addFriend(this.friendModel).subscribe(friend => alert("s"), error => alert("it didn't work"));
-    
+    this.addFriendService.addFriend(this.friendModel).subscribe(friend => alert('Your data was added. Nice getting to know you'), error => alert("it didn't work"));
+    this.getData('http://localhost:9001/allFriends')
+  }
+
+  public async getData(url: string): Promise <any>{
+    await fetch(url, {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'}
+    }).then(response => {
+      return response.json()
+    }).then(data => this.allFriends = data);
+    return;
+  }
+
+  ngOnInit(): any {
+    this.getData('http://localhost:9001/allFriends');
   }
 
   constructor(private addFriendService: AddFriendService){
   }
+  
 }
--- first-angular-exercise/src/app/friend.ts
+++ first-angular-exercise/src/app/friend.ts
@@ -4,12 +4,14 @@ export class Friend {
     email: string;
     phoneNumber: string;
     chosenLanguage: string;
+    customMessage: string;
 
-    public constructor(firstname: string, lastname: string, email: string, phoneNumber: string, chosenLanguage: string){
+    public constructor(firstname: string, lastname: string, email: string, phoneNumber: string, chosenLanguage: string, customMessage: string){
         this.firstname = firstname;
         this.lastname = lastname;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.chosenLanguage = chosenLanguage;
+        this.customMessage = customMessage;
     }
 }
